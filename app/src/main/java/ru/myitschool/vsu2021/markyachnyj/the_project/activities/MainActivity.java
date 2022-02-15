8bb41d7eb2b936330521587d684a20993ac4b150
MarkYachnyy <markyachny@gmail.com>
2022-02-15 23:12:16 +0300

    A

---
+++ app/src/main/java/ru/myitschool/vsu2021/markyachnyj/the_project/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.myitschool.vsu2021.markyachnyj.the_project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.myitschool.vsu2021.markyachnyj.the_project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i=0;i<5;i++){
+
+        }
+    }
+}
---
+++ app/src/main/java/ru/myitschool/vsu2021/markyachnyj/the_project/graphics/AnimatedBGView.java
@@ -0,0 +1,142 @@
+package ru.myitschool.vsu2021.markyachnyj.the_project.graphics;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class AnimatedBGView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Bitmap bitmap;
+    private int Height;
+    private int Width;
+    private int step_x;
+    private int step_y;
+    private int offset_x;
+    private int offset_y;
+    private int step_time_ms;
+    private DrawThread drawThread;
+    private Rect src_rect;
+    private int image_width;
+    private int image_height;
+
+    public AnimatedBGView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+        drawThread = new DrawThread();
+        offset_x=0;
+        offset_y=0;
+        step_x=1;
+        step_y=1;
+        step_time_ms=11;
+    }
+
+    public void setMotionParams(int step_x, int step_y, int step_time_ms){
+        this.step_x = step_x;
+        this.step_y = step_y;
+        this.step_time_ms = step_time_ms;
+    }
+
+    public void setDrawable(Bitmap drawable){
+        bitmap = drawable;
+        src_rect = new Rect(0,0,bitmap.getWidth(),bitmap.getHeight());
+        image_width = Width/2;
+        image_height = bitmap.getHeight()*image_width/bitmap.getWidth();
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Height = h;
+        Width = w;
+        if(h!=0 && w!=0){
+            image_width = Width/2;
+            image_height = bitmap.getHeight()*image_width/bitmap.getWidth();
+            drawThread.ready=true;
+        } else {
+            drawThread.request_stop();
+            drawThread.ready = false;
+        }
+    }
+
+    public void activate(){
+        if(drawThread!=null){
+            if(drawThread.ready){
+                drawThread.running=true;
+                drawThread.start();
+            }else{
+                Thread t = new Thread(){
+                    @Override
+                    public void run(){
+                        while (!drawThread.ready){
+
+                        }
+                        drawThread.running=true;
+                        drawThread.start();
+                    }
+                };
+                t.start();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        if(drawThread!=null){
+            drawThread.request_stop();
+        }
+    }
+    private class DrawThread extends Thread{
+        private boolean running = false;
+        private boolean ready = false;
+        @Override
+        public void run() {
+            while (running){
+                Canvas canvas = getHolder().lockCanvas();
+                if(canvas!=null){
+                    try{
+                        for(int i=-offset_x;i<Width;i+=image_width){
+                            for(int j=-offset_y;j<Height;j+=image_height){
+                                canvas.drawBitmap(bitmap,src_rect,new Rect(i,j,image_width+i,image_height+j),new Paint());
+                            }
+
+                        }
+
+                    }finally {
+                        getHolder().unlockCanvasAndPost(canvas);
+                    }
+                }
+                move();
+                try{
+                    Thread.sleep(step_time_ms);
+                } catch(InterruptedException e) {
+
+                }
+            }
+        }
+        public void request_stop(){
+            running = false;
+        }
+    }
+    private void move(){
+        offset_x+=step_x;
+        offset_y+=step_y;
+        offset_x%=image_width;
+        offset_y%=image_height;
+    }
+}
