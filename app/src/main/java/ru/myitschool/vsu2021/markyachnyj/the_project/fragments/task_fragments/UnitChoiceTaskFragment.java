d08a8c665f998a67daef8fb254161a5bf2b749f2
MarkYachnyy <markyachny@gmail.com>
2022-04-29 15:47:07 +0300

    a

--- app/src/main/java/ru/myitschool/vsu2021/markyachnyj/the_project/fragments/task_fragments/UnitChoiceTaskFragment.java
+++ app/src/main/java/ru/myitschool/vsu2021/markyachnyj/the_project/fragments/task_fragments/UnitChoiceTaskFragment.java
@@ -51,6 +51,7 @@ public class UnitChoiceTaskFragment extends TaskFragment {
                 ((TestSolverActivity)getActivity()).GiveAnswer(task, true);
             });
         }
+
         return view;
     }
 
